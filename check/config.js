4e958f744855f088155fa0a4f7cc6452039ffd3a
forcepower2025 <2025.forcepower@gmail.com>
2026-06-26 05:50:18 +0800

    Create config.js

---
+++ check/config.js
@@ -0,0 +1,4 @@
+const FRONTEND_CONFIG = {
+  LIFF_ID: '2010516604-EKrvHpLU', // 你的正式 LIFF ID
+  API_URL: 'https://script.google.com/macros/s/AKfycbx6eVNSLqgAobwxABdhLTfW3XePQwE07S8z4g2Vk4vzSg4g3aUKn3keZEQY2vmta3IN/exec' // 剛剛產出的正式 API 網址
+};
